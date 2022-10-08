5b5dd38054ddc93e7b1843db061ba809a792bddc
DanDChoi <showbegin622@gmail.com>
2022-10-08 15:48:00 +0900

    Spring batch basic1

--- src/test/java/dan/springbatch/SpringBatchApplicationTests.java
+++ src/test/java/dan/springbatch/SpringBatchApplicationTests.java
@@ -1,10 +1,10 @@
-package dan.shopping;
+package dan.springbatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppingApplicationTests {
+class SpringBatchApplicationTests {
 
 	@Test
 	void contextLoads() {
